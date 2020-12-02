7a40d6e080429a33c10f59b2547ab4ea7deb8d88
Jay Heng <jie.heng@nxp.com>
2020-12-02 21:16:11 +0800

    Updated implementation with GUI
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- env/do_setup_by_pip.bat
+++ env/do_setup_by_pip.bat
@@ -1,4 +1,4 @@
-pip.exe install PyQt5==5.15.2 
+pip.exe install PyQt5==5.15.1 
 pip.exe install pyqt5-tools==5.15.1.3
 pip.exe install openpyxl=3.0.5
 pip.exe install wordcloud=1.8.1
--- src/main.py
+++ src/main.py
@@ -4,43 +4,96 @@ import sys
 import os
 import time
 import random
+from PyQt5.Qt import *
+from lottery_win import *
 import openpyxl
 import wordcloud
 
-LUCKY_PEOPLE = 5
-g_luckyPersonIdx = []
+class lotteryMain(QMainWindow, Ui_MainWindow):
 
-def main(argv=None):
-    # Get people list from xlsx
-    wb = openpyxl.load_workbook(u"../dat/2020_11_30_fireDAP.xlsx")
-    ws = wb.active
+    def __init__(self, parent=None):
+        super(lotteryMain, self).__init__(parent)
+        self.setupUi(self)
+        self._register_callbacks()
+        self.peopleDat = None
+        self.luckyPeople = 0
+        self._show_default_board()
 
-    # Save all people names in txt
-    txt = u""
-    for i in range(1, ws.max_row):
-        txt += " " + ws.cell(row=i, column=3).value
+    def _register_callbacks(self):
+        self.pushButton_load.clicked.connect(self.callbackDoLoad)
+        self.pushButton_go.clicked.connect(self.callbackDoGo)
 
-    # Generate word cloud picture by txt
-    wc = wordcloud.WordCloud(width = 900,
-                             height = 383,
-                             font_path = "msyh.ttc",
-                             max_words = ws.max_row,
-                             background_color = "white")
-    wc.generate(txt)
-    wc.to_file("all_people.png")
+    def _show_default_board(self):
+        picFile = os.path.join(os.getcwd(), 'people_cloud_blank.png')
+        picObj = QtGui.QPixmap(picFile).scaled(self.label_showPeople.width(), self.label_showPeople.height())
+        self.label_showPeople.setPixmap(picObj)
 
-    # Get lucky people
-    global g_luckyPeopleIdx
-    g_luckyPeopleIdx = []
-    while len(g_luckyPeopleIdx) < LUCKY_PEOPLE:
-        idx = random.randint(1, ws.max_row)
-        if idx not in g_luckyPeopleIdx:
-            g_luckyPeopleIdx.append(idx)
-            print(idx, ws.cell(row=idx, column=3).value)
-            time.sleep(1)
+    def callbackDoLoad(self):
+        # Select xlsx file
+        self.srcXlsx, dummyType = QtWidgets.QFileDialog.getOpenFileName(self, u"Browse File", os.getcwd(), "All Files(*);;Source Files(*.xlsx)")
+        self.lineEdit_srcXlsx.setText(self.srcXlsx)
 
-    wb.close()
+        # Get people list from xlsx
+        wb = openpyxl.load_workbook(self.srcXlsx)
+        ws = wb.active
+
+        # Save all people names in txt
+        txt = u""
+        for i in range(1, ws.max_row):
+            txt += " " + ws.cell(row=i, column=3).value
+
+        # Generate word cloud picture by txt
+        wc = wordcloud.WordCloud(width = 700,
+                                 height = 297,
+                                 font_path = "msyh.ttc",
+                                 max_words = ws.max_row,
+                                 background_color = "white")
+        wc.generate(txt)
+        picFile = os.path.join(os.getcwd(), 'people_cloud.png')
+        wc.to_file(picFile)
+
+        # Show word cloud picture
+        picObj = QtGui.QPixmap(picFile).scaled(self.label_showPeople.width(), self.label_showPeople.height())
+        self.label_showPeople.setPixmap(picObj)
+
+        self.peopleDat = wb
+
+    def callbackDoGo(self):
+        self.textEdit_luckyPeopleBoard.clear()
+
+        if self.peopleDat == None:
+            QMessageBox.about(self, u"Info", u"Please select people data file (.xlsx)! " )
+            return
+
+        wb = self.peopleDat
+        ws = wb.active
+
+        if self.lineEdit_luckyPeople.text() != "":
+            self.luckyPeople = int(self.lineEdit_luckyPeople.text())
+
+        # Check lucky people number
+        if self.luckyPeople == 0 or self.luckyPeople > ws.max_row:
+            QMessageBox.about(self, u"Info", u"Please set proper lucky people! " )
+            return
+
+        # Get lucky people
+        luckyPeopleIdx = []
+        while len(luckyPeopleIdx) < self.luckyPeople:
+            idx = random.randint(1, ws.max_row)
+            if idx not in luckyPeopleIdx:
+                luckyPeopleIdx.append(idx)
+                #print(idx, ws.cell(row=idx, column=3).value)
+                strIdx = str(idx)
+                while len(strIdx) < len(str(ws.max_row)):
+                    strIdx = '0' + strIdx 
+                self.textEdit_luckyPeopleBoard.append(strIdx + " -> " + ws.cell(row=idx, column=3).value + "\r\n")
+                time.sleep(1)
 
 if __name__ == '__main__':
-    sys.exit(main())
+    app = QApplication(sys.argv)
+    mainWin = lotteryMain()
+    mainWin.setWindowTitle(u"pzh-lottery v1.0")
+    mainWin.show()
+
+    sys.exit(app.exec_())
 
